c2ca70cd77c91a3a09d36659e71ca00647da7c95
Henri Nieminen <henri@purkki.fi>
2018-07-14 01:25:47 +0300

    Add method for fetching story comments

--- Purkki.HackerNews.CLI/ApiClient.cs
+++ Purkki.HackerNews.CLI/ApiClient.cs
@@ -14,6 +14,7 @@ namespace Purkki.HackerNews.CLI
         private readonly HttpClient _client;
         private IList<long> _topStoryIds;
         private IDictionary<long, Story> _topStories;
+        private IDictionary<long, Comment> _comments;
 
         public int StoryCount => _topStoryIds?.Count ?? 0;
 
@@ -22,6 +23,7 @@ namespace Purkki.HackerNews.CLI
             _endpoint = endpoint;
             _client = new HttpClient();
             _topStories = new Dictionary<long, Story>();
+            _comments = new Dictionary<long, Comment>();
         }
 
         public async Task RefreshTopStoryIdsAsync()
@@ -31,6 +33,7 @@ namespace Purkki.HackerNews.CLI
             var json = await response.Content.ReadAsStringAsync();
             _topStoryIds = JsonConvert.DeserializeObject<List<long>>(json);
             _topStories.Clear();
+            _comments.Clear();
         }
 
         public async Task<IList<Story>> FetchTopStoriesAsync(int skip, int count)
@@ -64,11 +67,67 @@ namespace Purkki.HackerNews.CLI
             return stories;
         }
 
+        public async Task<IList<Comment>> FetchStoryCommentsAsync(long storyId)
+        {
+            var comments = new List<Comment>();
+            foreach (var commentId in _topStories[storyId].CommentIds)
+            {
+                comments.Add(await FetchCommentAsync(commentId));
+            }
+
+            return comments;
+        }
+
+        private async Task<Comment> FetchCommentAsync(long commentId)
+        {
+            Comment comment;
+            if (_comments.ContainsKey(commentId))
+            {
+                comment = _comments[commentId];
+            }
+            else
+            {
+                var request = new HttpRequestMessage(HttpMethod.Get, _endpoint + $"item/{commentId}.json");
+                var response = await _client.SendAsync(request);
+                var json = await response.Content.ReadAsStringAsync();
+                var dto = JsonConvert.DeserializeObject<CommentDTO>(json);
+                comment = new Comment
+                {
+                    Id = dto.Id,
+                    Text = dto.Text,
+                    Created = DateTimeOffset.FromUnixTimeSeconds(dto.Time).ToLocalTime(),
+                    Creator = dto.By,
+                    CommentIds = dto.Kids
+                };
+                _comments.Add(commentId, comment);
+            }
+
+            if (comment.CommentIds != null)
+            {
+                comment.Comments = new List<Comment>();
+                foreach (var id in comment.CommentIds)
+                {
+                    comment.Comments.Add(await FetchCommentAsync(id));
+                }
+            }
+
+            return comment;
+        }
+
         private class StoryDTO
         {
             public long Id { get; set; }
             public string Title { get; set; }
             public long Time { get; set; }
+            public string By { get; set; }
+            public IList<long> Kids { get; set; }
+        }
+
+        private class CommentDTO
+        {
+            public long Id { get; set; }
+            public long Time { get; set; }
+            public string Text { get; set; }
             public string By { get; set; }
             public IList<long> Kids { get; set; }
         }
---
+++ Purkki.HackerNews.CLI/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Purkki.HackerNews.CLI.Models
+{
+    public class Comment
+    {
+        public long Id { get; set; }
+        public string Text { get; set; }
+        public DateTimeOffset Created { get; set; }
+        public string Creator { get; set; }
+        public IList<Comment> Comments { get; set; }
+        public IList<long> CommentIds { get; set; }
+    }
+}
